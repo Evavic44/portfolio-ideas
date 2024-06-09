5dba1f5cf9a4fc90a371d9b8fc3d23e53b99c3c1
Victor Eke <evavic44@gmail.com>
2024-06-09 10:56:03 +0100

    remove duplicate script and fix action (#449)
    * add prettier ignore file
    
    * format file and update config
    
    * update home config
    
    * add prettierignore config
    
    * :robot: update auto-deploy action
    
    * make script file executable
    
    * bump checkout action to version 4
    
    * remove duplicate script and fix action

--- extract.sh
+++ extract.sh
@@ -13,18 +13,3 @@ echo "" >> "$output_file"
 
 # Copy portfolio table
 grep -P '^\|.*\|$' "$input_file" >> "$output_file"
-# Input and output files
-input_file="README.md"
-output_file="./docs/portfolio.md"
-
-# Clear the output file
-> "$output_file"
-
-# set vitepress layout 
-echo --- >> "$output_file"
-echo layout: home >> "$output_file"
-echo --- >> "$output_file"
-echo "" >> "$output_file"
-
-# Copy portfolio table
-grep -P '^\|.*\|$' "$input_file" >> "$output_file"
